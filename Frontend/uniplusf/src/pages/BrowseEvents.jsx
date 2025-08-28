553aad5f4f68c13894f0036a3d7a3986fdc914eb
Hasmitha Gunarathne <gdhasmithag@gmail.com>
2025-08-28 17:48:12 +0530

    Login is implemented in frontend with backend

---
+++ Frontend/uniplusf/src/pages/BrowseEvents.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function BrowseEvents() {
+  return (
+    <main className="max-w-8xl mx-auto px-6 py-10">
+      <h1 className="text-3xl font-bold">Browse Events</h1>
+      <p className="mt-3 text-white/80">Search & filter events (coming next).</p>
+    </main>
+  );
+}
