61b1a8c95e7e7841624d68147c76207e8ba7c9af
Hasmitha Gunarathne <gdhasmithag@gmail.com>
2025-09-21 05:06:09 +0530

    feat: Update CI configuration and add database setup for testing

--- Backend/uniplus/src/test/java/com/ruhuna/uniplus/UniplusApplicationTests.java
+++ Backend/uniplus/src/test/java/com/ruhuna/uniplus/UniplusApplicationTests.java
@@ -1,13 +1,11 @@
 package com.ruhuna.uniplus;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UniplusApplicationTests {
 
-    @Disabled("Disabled in CI due to missing DB")
 	@Test
 	void contextLoads() {
 	}
